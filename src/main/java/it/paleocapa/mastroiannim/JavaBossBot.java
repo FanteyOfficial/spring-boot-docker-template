62cd5dcab5bcb679db6f6819ca59f947d58efe21
FanteyOfficial <lorida1602@gmail.com>
2023-05-09 08:26:08 +0200

    Update

--- src/main/java/it/paleocapa/mastroiannim/JavaBossBot.java
+++ src/main/java/it/paleocapa/mastroiannim/JavaBossBot.java
@@ -2,59 +2,60 @@ package it.paleocapa.mastroiannim;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+
 @Service
 public class JavaBossBot extends TelegramLongPollingBot {
 
-	@Autowired
-    private Environment env;
-
 	private static final Logger LOG = LoggerFactory.getLogger(JavaBossBot.class);
-	 
-	@Value("${telegram.username}") 
-	private String botUsername;
-    
-	@Value("${telegram.token}") 
-	private String botToken;
 
+	private String botUsername;
+	private static String botToken;
+	private static JavaBossBot instance;
+
+	public static JavaBossBot getJavaBossBotInstance(String botUsername, String botToken){
+		if(instance == null) {
+			instance = new JavaBossBot();
+			instance.botUsername = botUsername;
+			JavaBossBot.botToken = botToken;
+		}
+		return instance;
+	}
+
+	private JavaBossBot(){
+		super(botToken);
+	}
+
+	@Override
+	public String getBotToken() {
+		return botToken;
+	}
+	
+	@Override
 	public String getBotUsername() {
-		//LOG.info(env.getProperty("botUsername"));
-		LOG.info(botUsername);
-		//LOG.info(System.getenv("telegram.username"));
 		return botUsername;
 	}
 
 	@Override
-	@Deprecated
-	public String getBotToken() {
-		//LOG.info(env.getProperty("botToken"));
-		LOG.info(botToken);
-		//LOG.info(System.getenv("telegram.token"));
-		return botToken;
-	}
-
 	public void onUpdateReceived(Update update) {
-		String command = update.getMessage().getText();
-
-        if (command.equals("/run")) {
-            String message = "Magari hai trovato un errore, magari no";
-            SendMessage response = new SendMessage();
-            response.setChatId(update.getMessage().getChatId().toString());
-            response.setText(message);
-
-            try {
-                execute(response);
-            } catch (TelegramApiException E) {
-                E.printStackTrace();
-            }
-        }
+		if (update.hasMessage() && update.getMessage().hasText()) {
+			
+			long chatId = update.getMessage().getChatId();
+			
+			SendMessage message = new SendMessage();
+			message.setChatId(chatId);
+			message.setText("Benvenuto! Come posso aiutarti?");
+			
+			try {
+				execute(message);
+			} catch (TelegramApiException e) {
+				LOG.error(e.getMessage());
+			}
+		}
 	}
 }
